8d98680c5141bc0515a0da150a28c2c14601da44
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-06-25 09:13:58 +0000

    5. Кэширование и KeepUnusedDataFor

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -5,6 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task"],
+  keepUnusedDataFor: 5,
   baseQuery: async (args, api, extraOptions) => {
     const result = await fetchBaseQuery({
       baseUrl: import.meta.env.VITE_BASE_URL,
