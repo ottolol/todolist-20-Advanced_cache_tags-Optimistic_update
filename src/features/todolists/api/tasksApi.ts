72a5377d36d0add4a695f3dd4c4151cfd5bb0851
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-06-25 09:44:35 +0000

    10. selectCachedArgsForQuery Если перейти на другую страницу тасок, optimistic update не работает.
    selectCachedArgsForQuery извлекает аргументы запросов, закэшированных в Redux Store. Это позволяет получить все аргументы для запросов, имеющих активные кэши. Это полезно для отслеживания того, какие запросы были сделаны и с какими параметрами, и для повторного использования этих параметров при необходимости:
    
    Результат: optimistic update отрабатывает с query-параметрами

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -40,19 +40,32 @@ export const tasksApi = baseApi.injectEndpoints({
         method: "PUT",
         body: model,
       }),
-      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled }) {
-        const patchResult = dispatch(
-          tasksApi.util.updateQueryData('getTasks', { todolistId, params: { page: 1 } }, state => {
-            const index = state.items.findIndex(task => task.id === taskId)
-            if (index !== -1) {
-              state.items[index] = { ...state.items[index], ...model }
-            }
-          })
-        )
+      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled, getState }) {
+        const cachedArgsForQuery = tasksApi.util.selectCachedArgsForQuery(getState(), 'getTasks')
+ 
+        let patchResults: any[] = []
+        cachedArgsForQuery.forEach(({ params }) => {
+          patchResults.push(
+            dispatch(
+              tasksApi.util.updateQueryData(
+                'getTasks',
+                { todolistId, params: { page: params.page } },
+                state => {
+                  const index = state.items.findIndex(task => task.id === taskId)
+                  if (index !== -1) {
+                    state.items[index] = { ...state.items[index], ...model }
+                  }
+                }
+              )
+            )
+          )
+        })
         try {
           await queryFulfilled
         } catch {
-          patchResult.undo()
+          patchResults.forEach(patchResult => {
+            patchResult.undo()
+          })
         }
       },
       invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
